66a9af23b66149f650309db5030eec532f886265
Matheus Batista <mbatista.sarti@gmail.com>
2024-03-04 13:46:20 -0300

    update

---
+++ ApiCatalogo/Models/Category.cs
@@ -0,0 +1,8 @@
+namespace ApiCatalogo.Models;
+
+public class Category
+{
+    public int CategoryId { get; set;}
+    public string? Name { get; set; }
+    public string? UrlImage { get; set; }
+}
---
+++ ApiCatalogo/Models/Product.cs
@@ -0,0 +1,12 @@
+namespace ApiCatalogo.Models;
+
+public class Product
+{
+    public int ProductId { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+    public string? UrlImage { get; set; }
+    public float Stock { get; set; }
+    public DateTime RegisterDate { get; set; }
+}
