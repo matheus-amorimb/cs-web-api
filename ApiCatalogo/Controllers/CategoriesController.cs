2b19ce3d8f42d0398bb788188ae8bb68bbffc4e3
Matheus Batista <mbatista.sarti@gmail.com>
2024-03-06 19:24:30 -0300

    CategoriesController get methods

---
+++ ApiCatalogo/Controllers/CategoriesController.cs
@@ -0,0 +1,51 @@
+using ApiCatalogo.Context;
+using ApiCatalogo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        
+        public CategoriesController(AppDbContext context)
+        {
+            _context = context;
+        }
+        
+        [HttpGet]
+        public ActionResult<ICollection<Category>> GetAllCategories()
+        {
+            var categories = _context.Categories.ToList();
+            if (categories is null)
+            {
+                return NotFound();
+            }
+            return categories;
+        }
+
+        [HttpGet("{id:int}", Name = nameof(GetCategory))]
+        public ActionResult<Category> GetCategory(int id)
+        {
+            var category = _context.Categories.FirstOrDefault(item => item.CategoryId == id);
+            if (category is null)
+            {
+                return NotFound();
+            }
+            return category;
+        }
+
+        [HttpPost]
+        public ActionResult InsertCategory(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            
+            return new CreatedAtRouteResult(nameof(GetCategory), 
+                new {id = category.CategoryId}, category); 
+        }
+    }
+}
