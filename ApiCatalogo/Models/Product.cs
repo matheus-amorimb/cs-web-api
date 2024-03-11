fb2262664d1cbcafdeef04688290f4b96b7ada43
Matheus Batista <mbatista.sarti@gmail.com>
2024-03-11 12:14:01 -0300

    inserting new feat

--- ApiCatalogo/Models/Product.cs
+++ ApiCatalogo/Models/Product.cs
@@ -1,17 +1,20 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
+using ApiCatalogo.Validation;
 
 namespace ApiCatalogo.Models;
 
 [Table("Products")]
-public class Product
+public class Product : IValidatableObject
 {
     [Key]
+    [JsonIgnore]
     public int ProductId { get; set; }
     
     [Required]
     [StringLength(80)]
+    // [FirstLetterUppercase]
     public string? Name { get; set; }
     
     [Required]
@@ -32,4 +35,23 @@ public class Product
     
     [JsonIgnore]
     public Category? Category { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (!string.IsNullOrEmpty(this.Name))
+        {
+            var firstLetter = this.Name[0].ToString();
+            if (firstLetter != firstLetter.ToUpper())
+            {
+                yield return new ValidationResult("First letter must be capitalized", 
+                    new[] { nameof(this.Name) });
+            }
+        }
+
+        if (this.Stock <= 0)
+        {
+            yield return new ValidationResult("Stock must be greater than zero", 
+                new[] { nameof(this.Stock) });
+        }
+    }
 }
--- ApiCatalogo/Validation/FirstLetterUppercaseAttribute.cs
+++ ApiCatalogo/Validation/FirstLetterUppercaseAttribute.cs
@@ -4,4 +4,21 @@ namespace ApiCatalogo.Validation;
 
 public class FirstLetterUppercaseAttribute : ValidationAttribute
 {
+    protected override ValidationResult IsValid(object? value, 
+        ValidationContext validationContext)
+    {
+        if (value == null || string.IsNullOrEmpty(value.ToString()))
+        {
+            return ValidationResult.Success;
+        }
+
+        var firstLetter = value.ToString()[0].ToString();
+        if (firstLetter != firstLetter.ToUpper())
+        {
+            return new ValidationResult("First letter must be capitalizez");
+        }
+        
+        return ValidationResult.Success;
+
+    }
 }
