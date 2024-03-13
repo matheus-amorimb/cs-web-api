388780465efd8f7001e76153dd6befa2d2d581b1
Matheus Batista <mbatista.sarti@gmail.com>
2024-03-13 08:58:27 -0300

    inserting new feat

--- ApiCatalogo/obj/Debug/net8.0/ApiCatalogo.AssemblyInfo.cs
+++ ApiCatalogo/obj/Debug/net8.0/ApiCatalogo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiCatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95da47c881a2dc9c4ba736251b267193646cc244")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9c82e68656b9df3314a84bf7059bad3d12dd293")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiCatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiCatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
