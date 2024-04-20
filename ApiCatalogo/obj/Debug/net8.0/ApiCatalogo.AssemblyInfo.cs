649cd005d6fecdab67cc2f5936088b714ca2382e
Matheus Batista <mbatista.sarti@gmail.com>
2024-04-20 10:22:47 -0300

    :feat:

--- ApiCatalogo/obj/Debug/net8.0/ApiCatalogo.AssemblyInfo.cs
+++ ApiCatalogo/obj/Debug/net8.0/ApiCatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiCatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de0d01c5810bb16301d1f9732c228f36f9b39395")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f45da945636ae2b04ec67a8748b801dd03adac3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiCatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiCatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
