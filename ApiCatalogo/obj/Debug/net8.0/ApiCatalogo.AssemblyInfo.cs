5a358b93d6e3f8b5ff21b26ac031358f73755427
Matheus Batista <mbatista.sarti@gmail.com>
2024-04-21 15:33:03 -0300

    :feat: adding endpoint AddUserToRole to controller AuthController

--- ApiCatalogo/obj/Debug/net8.0/ApiCatalogo.AssemblyInfo.cs
+++ ApiCatalogo/obj/Debug/net8.0/ApiCatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiCatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86f9bef70b9b7ba79bd445b78599008f90631067")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86e8515b58f895317aac90a14282d18792f8cfcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiCatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiCatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
